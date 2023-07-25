7ac961b54931ba9645e070f61e9ede2afd763acd
unknown <ruchithamangi1985@gmail.com>
2023-07-25 16:56:05 +0530

    addedd

---
+++ QuickStart.java
@@ -0,0 +1,8 @@
+
+class QuickStart {
+    public static void main (String[] args) {
+        System.out.println("Hello, World.");
+        int value=123;
+        System.out.println("hello "+value);
+    }
+}
---
+++ prac/one.java
@@ -0,0 +1,9 @@
+
+class one
+{
+    public static void main(String args)
+    {
+        int x = 200;
+        System.out.println("hello world");
+    }
+}
