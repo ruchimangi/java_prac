359b926efd5220682d5fe0ffa67cfe58f54a6c8a
unknown <ruchithamangi1985@gmail.com>
2023-07-28 22:13:45 +0530

    python texts

---
+++ git.py
@@ -0,0 +1,7 @@
+"""
+1. git add .    [adds files to staging area] step-1
+2. git commit -m "message"  [commits files to local repository]
+3.git push origin main [pushes to github repo]
+"""
+def main():
+    pass
