eff210bb011e36039e2a2e87335c68057d380e99
unknown <ruchithamangi1985@gmail.com>
2023-08-01 23:16:17 +0530

    problem2 answer

--- week1/problem1.java
+++ week1/problem1.java
@@ -1,5 +1,19 @@
 package week1;
 
 public class problem1 {
+    public static void main(String[] args)
+    {
+        int[] arr = new int[] {1,2,3,4,5,6,7,8};
+
+        int sum[];
+
+        for(int i=0; i<arr.length; i++)
+        {
+            sum = sum + arr[i];
+        }
+
+        System.out.println("sum of array is:"+sum);
+        
+    }
     
 }
---
+++ week1/problem2.java
@@ -0,0 +1,22 @@
+package week1;
+import java.util.Arrays;
+
+public class problem2 {
+    public static void main(String[] args){
+
+       int[] arr = {10, 20, 30, 40, 50};
+
+        int count = 0;
+        for(int i=0; i<arr.length; i++) 
+        {
+            count = count+1;
+        }
+
+    System.out.println("Number of elements in the array: " + count);
+    
+    }
+
+
+}
+    
+
